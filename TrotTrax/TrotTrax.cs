4a0c7e9ae19b0456911f9ced5850b718c27e3fc6
kbrimm <kbrimm@pdx.edu>
2015-07-07 12:06:52 -0700

    License Inclusion
    Updated files to include license information.

--- TrotTrax/TrotTrax.cs
+++ TrotTrax/TrotTrax.cs
@@ -1,4 +1,11 @@
-﻿using System;
+﻿/* 
+ * TrotTrax
+ *     Copyright (c) 2015 Katy Brimm
+ *     This source file is licensed under the GNU General Public License. Please see the file LICENSE in this distribution for license terms.
+ * Contact: kbrimm@pdx.edu
+ */
+
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
